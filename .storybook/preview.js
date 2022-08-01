c452fbae704d5444f935b8ff49eaf76b74ef33fd
ddfilipov <denisfilipov12+3@gmail.com>
2022-08-01 16:53:48 +0200

    storybook working now!

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
+import { MemoryRouter } from "react-router-dom";
+import { addDecorator } from "@storybook/react";
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
+
+addDecorator((story) => <MemoryRouter initialEntries={["/"]}> {story()}</MemoryRouter>);
