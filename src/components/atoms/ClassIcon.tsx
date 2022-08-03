0b2aa17752c05e1b379254fdea631ee6f1520139
ddfilipov <denisfilipov12+3@gmail.com>
2022-08-03 16:59:38 +0200

    thinking about changing the ClassChooser

--- src/components/atoms/ClassIcon.tsx
+++ src/components/atoms/ClassIcon.tsx
@@ -1,17 +1,28 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export interface ClassIconProps {
     href: string;
     src: string;
     alt: string;
 }
+interface IBackgroundImage {
+    backgroundImage: string;
+}
 
+const ButtonStyled = styled.button<IBackgroundImage>`
+    height: 38px;
+    width: 38px;
+    background-image: url(${(props) => props.backgroundImage});
+    cursor: pointer;
+`;
 
 export const ClassIcon: FC<ClassIconProps> = ({ href, src, alt }) => {
     return (
         <Link to={href}>
-            <img src={src} alt={alt}></img>
+            {/* <img src={src} alt={alt}></img> */}
+            <ButtonStyled backgroundImage={src} />
         </Link>
     );
 };
