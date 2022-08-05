4d7c143681ccce79be19d4567abae5107fffb3bf
ddfilipov <denisfilipov12+3@gmail.com>
2022-08-05 17:41:48 +0200

    added new TalentTreeGrid component

---
+++ src/components/molecules/TalentTreeGrid.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: grid;
+    grid-template-rows: 2rem auto;
+    justify-self: stretch;
+    color: white;
+`;
+
+interface TalentTreeGridProps {
+
+}
+
+export const TalentTreeGrid: FC<TalentTreeGridProps> = ({
+
+}) => {
+
+    return (
+        <Container>
+    
+        </Container>
+    );
+};
