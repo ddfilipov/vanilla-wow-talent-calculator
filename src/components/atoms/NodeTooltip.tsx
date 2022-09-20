0e92bd55ea4e2d15ee42f6df1d64124922fe2106
ddfilipov <denisfilipov12+3@gmail.com>
2022-09-20 16:40:16 +0200

    got stuck, work harder

--- src/components/atoms/NodeTooltip.tsx
+++ src/components/atoms/NodeTooltip.tsx
@@ -16,10 +16,19 @@ interface NodeTooltipProps {
 }
 
 export const NodeTooltip: FC<NodeTooltipProps> = ({ nodeFields, currentNodeRank }) => {
+    const mapCurrentRank = () => {
+        return <p>a</p>;
+        // return <p>{nodeFields.ranks.find((rank) => console.log("helloooooo", rank))}</p>;
+        // return nodeFields.ranks.map((rank, index) => {
+        //     <p>{rank.rankDescription}</p>;
+        // });
+    };
+
     return (
         <Container>
             <div>{nodeFields.talentNodeName}</div>
             <div>Rank {currentNodeRank + 1}</div>
+            {/* {mapCurrentRank} if I uncomment this I get an error. Investigade*/}
         </Container>
     );
 };
