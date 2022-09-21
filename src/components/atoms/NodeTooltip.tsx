d0fae710ef4cd073fcb101092d089dbfd74a0f43
ddfilipov <denisfilipov12+3@gmail.com>
2022-09-21 16:21:16 +0200

    current rank can't be higher than the max ranks of the talent

--- src/components/atoms/NodeTooltip.tsx
+++ src/components/atoms/NodeTooltip.tsx
@@ -2,7 +2,11 @@ import { FC, useEffect, useState } from "react";
 import styled from "styled-components";
 import { ISpecTalents } from "../../interfaces";
 
-const Container = styled.div`
+interface NodeTooltipStyle {
+    maxTalentRanks: number;
+}
+
+const Container = styled.div<NodeTooltipStyle>`
     border: 1px solid white;
     background: rgba(0, 0, 0, 0.5);
     width: 120px;
@@ -16,6 +20,12 @@ interface NodeTooltipProps {
 }
 
 export const NodeTooltip: FC<NodeTooltipProps> = ({ nodeFields, currentNodeRank }) => {
+    let maxTalentRanks: number = nodeFields.ranks.length;
+
+    const getNextRank = () => {
+        return maxTalentRanks > currentNodeRank ? currentNodeRank + 1 : currentNodeRank;
+    };
+
     const mapCurrentRank = () => {
         return <p>a</p>;
         // return <p>{nodeFields.ranks.find((rank) => console.log("helloooooo", rank))}</p>;
@@ -25,9 +35,9 @@ export const NodeTooltip: FC<NodeTooltipProps> = ({ nodeFields, currentNodeRank 
     };
 
     return (
-        <Container>
+        <Container maxTalentRanks={nodeFields.ranks.length}>
             <div>{nodeFields.talentNodeName}</div>
-            <div>Rank {currentNodeRank + 1}</div>
+            <div>Rank {getNextRank()}</div>
             {/* {mapCurrentRank} if I uncomment this I get an error. Investigade*/}
         </Container>
     );
