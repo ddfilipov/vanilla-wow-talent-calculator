1f4ccc6469d7526c3e365643ff5d910f274aa59d
ddfilipov <denisfilipov12+3@gmail.com>
2022-09-23 22:27:52 +0200

    styling tooltip

--- src/components/atoms/NodeTooltip.tsx
+++ src/components/atoms/NodeTooltip.tsx
@@ -7,11 +7,22 @@ interface NodeTooltipStyle {
 }
 
 const Container = styled.div<NodeTooltipStyle>`
+    display: grid;
+    grid-template-columns: [c1-start] 40px [c2] 40px [c3-end];
+    grid-template-rows: [r1-start] 40px [r2] 40px [r3] 40px [r4];
     border: 1px solid white;
     background: rgba(0, 0, 0, 0.5);
-    width: 120px;
-    height: 35px;
-    z-index: 6;
+    z-index: 1000;
+    width: 300px;
+    height: 150px;
+`;
+
+const TalentNameStyled = styled.div`
+    grid-column-start: 1;
+    grid-column-end: 3;
+    grid-row-start: 1;
+    grid-row-end: 2;
+    background-color: red;
 `;
 
 interface NodeTooltipProps {
@@ -33,7 +44,7 @@ export const NodeTooltip: FC<NodeTooltipProps> = ({ nodeFields, currentNodeRank 
 
     return (
         <Container maxTalentRanks={nodeFields.ranks.length}>
-            <div>{nodeFields.talentNodeName}</div>
+            <TalentNameStyled>{nodeFields.talentNodeName}</TalentNameStyled>
             <div>Rank {getNextRank()}</div>
             {mapCurrentRank()}
         </Container>
