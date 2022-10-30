0bdb7edd11a7ad479562c52ac99078dec8a3b306
ddfilipov <denisfilipov12+3@gmail.com>
2022-10-30 17:26:00 +0100

    copied the array with the 2 props I need. Now gotta see how to add a new prop to the object

--- src/components/molecules/TalentTree.tsx
+++ src/components/molecules/TalentTree.tsx
@@ -109,11 +109,14 @@ export const TalentTree: FC<TalentTreeProps> = ({
     useEffect(() => {
         // TODO: should create something like a Map type that links talentId with current points spent.
         // need something that has talentId, pointsSpent, maxPoints
-        let newArray: INodeData[];
-        specRanks?.map((talent,index)=>{
-            console.log(talent);
-            // talent.talentNodeId
-    })
+        let newArray = specRanks?.map(({talentNodeId, numberOfRanks})=>(
+            {talentNodeId, numberOfRanks}
+        ))
+            console.log(newArray)
+    //     specRanks?.map((talent,index)=>{
+    //         console.log(talent);
+    //         // newArray.
+    // })
     }, []);
 
     return (
