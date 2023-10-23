a2f0e7d496287c0ae7eb6601acdd2aa4fe501a75
ddfilipov <denisfilipov12+3@gmail.com>
2023-10-23 21:33:39 +0200

    changed / dir title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
+
 import { MainPage } from "@/atomic/organisms/MainPage";
+
+export const metadata: Metadata = {
+    title: "Choose a class",
+};
 
 export default function Home() {
     return <MainPage />;
