cea570bbbd1bd76b8af24f42a0b0019580f3809d
ddfilipov <denisfilipov12+3@gmail.com>
2023-11-01 22:06:27 +0100

    renaming var

--- src/atomic/atoms/ClassIcon.tsx
+++ src/atomic/atoms/ClassIcon.tsx
@@ -29,12 +29,10 @@ const ButtonStyled = styled.button<IBackgroundImage>`
 `;
 
 export const ClassIcon: FC<ClassIconProps> = ({ href, src }) => {
-    const router = usePathname();
-    console.log("router:", router);
-    console.log("href:", href);
+    const currentPath = usePathname();
     return (
         <Link href={href}>
-            <ButtonStyled $backgroundImage={src} tabIndex={-1} href={href} $currentPath={router} />
+            <ButtonStyled $backgroundImage={src} tabIndex={-1} href={href} $currentPath={currentPath} />
         </Link>
     );
 };
