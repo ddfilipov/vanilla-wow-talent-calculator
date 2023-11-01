b7ffcbc71e5330bb8b9e9a6ce98d4a26acef7f2a
ddfilipov <denisfilipov12+3@gmail.com>
2023-11-01 19:04:28 +0100

    fixed console errors

--- src/atomic/atoms/ClassIcon.tsx
+++ src/atomic/atoms/ClassIcon.tsx
@@ -7,19 +7,19 @@ export interface ClassIconProps {
     src: string;
 }
 interface IBackgroundImage {
-    backgroundImage: string;
-    relativePath: string;
+    $backgroundImage: string;
+    $relativePath: string;
     href: string;
 }
 
 const ButtonStyled = styled.button<IBackgroundImage>`
     height: 38px;
     width: 38px;
-    background-image: url(${(props) => props.backgroundImage});
+    background-image: url(${(props) => props.$backgroundImage});
     border: 2px solid #212e46;
     cursor: pointer;
-    /* border: 2px solid ${(props) => props.href === props.relativePath && "var(--uncapped-node-color)"}; */
-    opacity: ${(props) => (props.href === props.relativePath ? 1 : 0.6666)};
+    /* border: 2px solid ${(props) => props.href === props.$relativePath && "var(--uncapped-node-color)"}; */
+    opacity: ${(props) => (props.href === props.$relativePath ? 1 : 0.6666)};
     &:hover {
         opacity: 1;
         box-shadow: inset 0 0 5px #596e92;
@@ -29,7 +29,7 @@ const ButtonStyled = styled.button<IBackgroundImage>`
 export const ClassIcon: FC<ClassIconProps> = ({ href, src }) => {
     return (
         <Link href={href}>
-            <ButtonStyled backgroundImage={src} tabIndex={-1} relativePath={location.pathname} href={href} />
+            <ButtonStyled $backgroundImage={src} tabIndex={-1} $relativePath={location.pathname} href={href} />
         </Link>
     );
 };
