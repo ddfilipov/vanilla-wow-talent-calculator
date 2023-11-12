6ef485c5ea93488242126029733b58eac7e3b12e
ddfilipov <denisfilipov12+3@gmail.com>
2023-11-12 21:52:07 +0100

    this one was hard to do

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { Inter } from "next/font/google";
 import styled from "styled-components";
 import GlobalStyle from "@/styles/GlobalStyles";
+import { breakpoints } from "@/styles/breakpoints";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,14 +24,17 @@ const Container = styled.div`
     border-radius: 0.2rem;
     border: 1px solid var(--main-area-border);
 
-    @media (width < 768px) {
+    @media ((${breakpoints.mobile})) {
+        background-color: green;
         min-width: 280px;
         min-height: 300px;
     }
-    @media (width >= 768px) {
+    @media ((${breakpoints.tablet})) {
+        background-color: blue;
         min-height: 500px;
     }
-    @media (width >= 1250px) {
+    @media ((${breakpoints.desktop})) {
+        background-color: red;
         max-width: 1200px;
         min-width: 750px;
         max-height: 800px;
---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,5 @@
+export const breakpoints = {
+    mobile: `min-width: ${0}`,
+    tablet: `min-width: ${768}`,
+    desktop: `min-width: ${1250}`,
+};
