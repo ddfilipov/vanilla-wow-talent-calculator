dc6f153915f577d207716a800508368d32c8955f
ddfilipov <denisfilipov12+3@gmail.com>
2023-12-13 20:28:18 +0100

    can't make this stupid arrows work

--- src/atomic/atoms/Arrow.tsx
+++ src/atomic/atoms/Arrow.tsx
@@ -23,12 +23,18 @@ const StyledTestArrow = styled.div<IArrow>`
     grid-column-start: ${(props) => props.$startingColumn};
     grid-column-end: ${(props) => props.$endingColumn + 1};
     background-color: #ffd100;
-    width: ${(props) => (props.$isVerticalArrow ? "15%" : "calc(85% - 38px)")};
-    height: ${(props) => (props.$isVerticalArrow ? "calc(85% - 38px)" : "15%")};
+    transform: ${(props) => (props.$isVerticalArrow ? "rotate(90deg)" : "none")};
+    width: 100%;
+    /* width: ${(props) => (props.$isVerticalArrow ? "100%" : "30%")}; */
+    height: 15px;
+    z-index: 4;
+    clip-path: polygon(0% 20%, 60% 20%, 60% 0%, 100% 50%, 60% 100%, 60% 80%, 0% 80%);
 `;
 
 export const Arrow: FC<ArrowProps> = ({ startingRow, endingRow, startingColumn, endingColumn }) => {
     const isVericalArrow = startingColumn - endingColumn === 0;
+    console.log("startingColumn:", startingColumn);
+    console.log("endingColumn:", endingColumn);
     return (
         <StyledTestArrow
             $startingRow={startingRow}
