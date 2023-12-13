efd8882698cc9cf4c5c1af28d10f26441500845a
ddfilipov <denisfilipov12+3@gmail.com>
2023-12-13 21:49:59 +0100

    getting somewhere with them arrows

--- src/atomic/atoms/Arrow.tsx
+++ src/atomic/atoms/Arrow.tsx
@@ -23,12 +23,24 @@ const StyledTestArrow = styled.div<IArrow>`
     grid-column-start: ${(props) => props.$startingColumn};
     grid-column-end: ${(props) => props.$endingColumn + 1};
     background-color: #ffd100;
-    transform: ${(props) => (props.$isVerticalArrow ? "rotate(90deg)" : "none")};
-    width: 100%;
-    /* width: ${(props) => (props.$isVerticalArrow ? "100%" : "30%")}; */
-    height: 15px;
     z-index: 4;
-    clip-path: polygon(0% 20%, 60% 20%, 60% 0%, 100% 50%, 60% 100%, 60% 80%, 0% 80%);
+    position: relative;
+    /* width: 100%; width of the rectangle part */
+    /* height: 50px; height of the rectangle part */
+
+    width: ${(props) => (props.$isVerticalArrow ? "12%" : "calc(85% - 38px)")};
+    height: ${(props) => (props.$isVerticalArrow ? "calc(85% - 38px)" : "12%")};
+`;
+const ArrowHead = styled.div<{ $isVerticalArrow: boolean }>`
+    position: absolute;
+    right: -10px; /* position the arrowhead */
+    top: ${(props) => (props.$isVerticalArrow ? "100%" : "50%")};
+    width: 0;
+    height: 0;
+    border-left: 10px solid #ffd100; /* size and color of the arrowhead */
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    transform: translateY(-50%); /* center the arrowhead */
 `;
 
 export const Arrow: FC<ArrowProps> = ({ startingRow, endingRow, startingColumn, endingColumn }) => {
@@ -42,6 +54,8 @@ export const Arrow: FC<ArrowProps> = ({ startingRow, endingRow, startingColumn, 
             $startingColumn={startingColumn}
             $endingColumn={endingColumn}
             $isVerticalArrow={isVericalArrow}
-        />
+        >
+            <ArrowHead $isVerticalArrow={isVericalArrow} />
+        </StyledTestArrow>
     );
 };
