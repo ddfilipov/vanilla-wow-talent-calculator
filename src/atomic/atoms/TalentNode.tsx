5ab77a770c5542fb8318b50c12c2ce4701fb583d
ddfilipov <denisfilipov12+3@gmail.com>
2023-12-19 22:48:48 +0100

    removed cue

--- src/atomic/atoms/TalentNode.tsx
+++ src/atomic/atoms/TalentNode.tsx
@@ -192,7 +192,6 @@ export const TalentNode: FC<TalentNodeProps> = ({
                 onMouseEnter={handleOnMouseEnter}
                 onMouseLeave={() => setIsHovered(false)}
             >
-                {nodeId}
                 <ButtonStyled
                     $backgroundImage={`/images/talent-icons/${src}.jpg`}
                     onClick={handleClick}
