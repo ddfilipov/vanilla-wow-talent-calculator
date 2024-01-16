0f9a84268855ed74bf1dcd5ad01fb73e9594d9a9
ddfilipov <denisfilipov12+3@gmail.com>
2024-01-16 17:56:07 +0100

    backend is set up

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,12 @@
-const express = require('express');
+import express, { Request, Response } from "express";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.get("/", (req, res) => {
+    res.send("Hello from the backend!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
