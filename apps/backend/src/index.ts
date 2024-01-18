4b7d2eb8716d6b2486ee35618f6e7d07f1e8df61
ddfilipov <denisfilipov12+3@gmail.com>
2024-01-18 18:27:28 +0100

    changed backend port to 5000 and toyed around package.jsons

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello from the backend!");
